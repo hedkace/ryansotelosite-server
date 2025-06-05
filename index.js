f3484ce3d94a04c16b81118283eafd86bf720369
aka: cauzality, zeno evil, ... <cauzality@comcast.net>
2025-06-05 09:10:38 -0500

    corrections

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 app.use(cors({
-    origin: "*",
+    origin: ["https://ryansotelo.site", "https://ryansotelo.work"],
     methods: ["GET", "POST"]
 }))
 
